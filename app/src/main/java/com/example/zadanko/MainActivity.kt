c41f364ab73bc081fe11fba1302a76b5f3721696
Jocheu <jotomaniak@gmail.com>
2022-01-12 11:07:07 +0100

    Pierwsza wersja

---
+++ app/src/main/java/com/example/zadanko/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.zadanko
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        var n = 0;
+        findViewById<Button>(R.id.buttonLeft).setOnClickListener {
+            if(n == 0){
+                n = 3;
+            }
+            else{
+                n = n-1;
+            }
+
+            if(n==0){
+                findViewById<ImageView>(R.id.imageView1).setImageResource(R.drawable.pop)
+            }
+            else if(n==1){
+                findViewById<ImageView>(R.id.imageView1).setImageResource(R.drawable.swole_doge)
+            }
+            else if(n==2){
+                findViewById<ImageView>(R.id.imageView1).setImageResource(R.drawable.musk)
+            }
+            else if(n==3){
+                findViewById<ImageView>(R.id.imageView1).setImageResource(R.drawable.jp2)
+            }
+        }
+
+        findViewById<Button>(R.id.buttonRight).setOnClickListener {
+            if(n == 3){
+                n = 0;
+            }
+            else{
+                n = n+1;
+            }
+
+            if(n==0){
+                findViewById<ImageView>(R.id.imageView1).setImageResource(R.drawable.pop)
+            }
+            else if(n==1){
+                findViewById<ImageView>(R.id.imageView1).setImageResource(R.drawable.swole_doge)
+            }
+            else if(n==2){
+                findViewById<ImageView>(R.id.imageView1).setImageResource(R.drawable.musk)
+            }
+            else if(n==3){
+                findViewById<ImageView>(R.id.imageView1).setImageResource(R.drawable.jp2)
+            }
+        }
+
+        findViewById<Button>(R.id.buttonRotationLeft).setOnClickListener {
+            var obrot = findViewById<EditText>(R.id.obrot).text.toString().toFloat()
+            findViewById<ImageView>(R.id.imageView1).rotation
+
+        }
+
+    }
+}
